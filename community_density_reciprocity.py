78d763ad34f2f200232edc7f9e680acf199c57ab
Matteo Mistri <matteo.mistri1996@gmail.com>
2019-05-29 11:24:07 +0200

    Now community density and reciprocity is full scriptable. Added results

--- community_density_reciprocity.py
+++ community_density_reciprocity.py
@@ -3,38 +3,44 @@ import pandas as pd
 import sys
 from os import walk
 import networkx as nx
-import csv
 
-file = sys.argv[1]
-time = int(sys.argv[2])
+path = sys.argv[1]
+save_path = sys.argv[2]
 
-M = nx.read_graphml(file)
+dir_files = []
+for (_, _, filenames) in walk(path):
+    dir_files.extend(filenames)
+    break
+all_files = [path + f for f in dir_files]
 
-for n1, n2, d in M.edges(data=True):
-	d.pop('edgetime', None)
+for time in range (0, 30):
+	M = nx.read_graphml(all_files[time])
 
-G = nx.DiGraph()
-for u,v,data in M.edges(data=True):
-    w = data['weight'] if 'weight' in data else 1.0
-    if G.has_edge(u,v):
-        G[u][v]['weight'] += w
-    else:
-        G.add_edge(u, v, weight=w)
+	for n1, n2, d in M.edges(data=True):
+		d.pop('edgetime', None)
 
-res = pd.DataFrame(columns=["alliance_name", "density", "reciprocity"])
+	G = nx.DiGraph()
+	for u,v,data in M.edges(data=True):
+	    w = data['weight'] if 'weight' in data else 1.0
+	    if G.has_edge(u,v):
+	        G[u][v]['weight'] += w
+	    else:
+	        G.add_edge(u, v, weight=w)
 
-for alliance in alliance_members:
-	community = list((alliance_members[alliance][time]))
-	if (len(community) > 9):
-		tmp = G.subgraph(community)
-		SG = nx.DiGraph(tmp)
-		for n in community:
-			if n not in SG.nodes:
-				SG.add_node(n)
-		try:
-			res.loc[len(res)] = [alliance, nx.density(SG), nx.overall_reciprocity(SG)]
+	res = pd.DataFrame(columns=["alliance_name", "density", "reciprocity"])
 
-		except:
-			res.loc[len(res)] = [alliance, 0 , 0]
+	for alliance in alliance_members:
+		community = list((alliance_members[alliance][time]))
+		if (len(community) > 9):
+			tmp = G.subgraph(community)
+			SG = nx.DiGraph(tmp)
+			for n in community:
+				if n not in SG.nodes:
+					SG.add_node(n)
+			try:
+				res.loc[len(res)] = [alliance, nx.density(SG), nx.overall_reciprocity(SG)]
 
-res.to_csv("community_density_reciprocity.csv", index=False)
+			except:
+				res.loc[len(res)] = [alliance, 0 , 0]
+
+	res.to_csv(save_path + "/trades_community_density_reciprocity" + str(time + 1) + ".csv", index=False)
