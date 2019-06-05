8c6ca97f1486ae22ac3b175581f3f6944003a2a2
Matteo Mistri <matteo.mistri1996@gmail.com>
2019-06-05 14:17:00 +0200

    Added refined version of spammer/side village

--- find_spammer_sidevillage.py
+++ find_spammer_sidevillage.py
@@ -3,12 +3,15 @@ import sys
 import networkx as nx
 import pandas as pd
 
-messages = sys.argv[1]
-trades = sys.argv[2]
+attacks = sys.argv[1]
+messages = sys.argv[2]
+trades = sys.argv[3]
 
+MG_attacks = nx.read_graphml(attacks)
 MG_messages = nx.read_graphml(messages)
 MG_trades = nx.read_graphml(trades)
 
+a_out_degree = MG_attacks.out_degree(weight = "weight")
 m_in_degree = MG_messages.in_degree(weight = "weight")
 m_out_degree = MG_messages.out_degree(weight = "weight")
 t_in_degree = MG_trades.in_degree(weight = "weight")
@@ -29,28 +32,44 @@ all_nodes = set(MG_messages.nodes).union(set(MG_trades.nodes))
 
 possible_spammer = all_nodes - community_set
 
-res = pd.DataFrame(columns=["node", "message_in", "message_out", "trade_in", "trade_out"])
+res = pd.DataFrame(columns=["node", "attacks_out","message_in", "message_out", "trade_in", "trade_out"])
+
+spammer = set()
 
 for n in possible_spammer:
-
 	if n in MG_messages.nodes:
 		if n in MG_trades.nodes:
 				if (m_in_degree[n] == 0 and m_out_degree[n] > 30) or ((m_in_degree[n] != 0 and m_out_degree[n] / m_in_degree[n]) > 10):
 					print(n + " is a possible spammer");
+					spammer.add(n)
 				if (t_in_degree[n] == 0 and t_in_degree[n] > 15) or ((t_in_degree[n] != 0 and t_out_degree[n] / t_in_degree[n]) > 5):
 					print(n + " is a possible side village")
+					spammer.add(n)
 		else:
 				if (m_in_degree[n] == 0 and m_out_degree[n] > 30) or ((m_in_degree[n] != 0 and m_out_degree[n] / m_in_degree[n]) > 10):
-					print(n + " is a possible spammer");
+					print(n + " is a possible spammer")
+					spammer.add(n)
 	else:
 		if (t_in_degree[n] == 0 and t_in_degree[n] > 15) or ((t_in_degree[n] != 0 and t_out_degree[n] / t_in_degree[n]) > 5):
 			print(n + " is a possible side village")
+			spammer.add(n)
 
+for n in spammer:
 	if n in MG_messages.nodes:
 		if n in MG_trades.nodes:
-			res.loc[len(res)] = [n, m_in_degree[n], m_out_degree[n], t_in_degree[n], t_out_degree[n]]
+			if n in MG_attacks:
+				res.loc[len(res)] = [n, a_out_degree[n],m_in_degree[n], m_out_degree[n], t_in_degree[n], t_out_degree[n]]
+			else:
+				res.loc[len(res)] = [n, 0,m_in_degree[n], m_out_degree[n], t_in_degree[n], t_out_degree[n]]
+			
 		else:
-			res.loc[len(res)] = [n, m_in_degree[n], m_out_degree[n], 0, 0]
+			if n in MG_attacks:
+				res.loc[len(res)] = [n, a_out_degree[n] ,m_in_degree[n], m_out_degree[n], 0, 0]
+			else:
+				res.loc[len(res)] = [n, 0 ,m_in_degree[n], m_out_degree[n], 0, 0]
 	else:
-		res.loc[len(res)] = [n, 0, 0, t_in_degree[n], t_out_degree[n]]
+		if n in MG_attacks:
+			res.loc[len(res)] = [n, a_out_degree[n], 0, 0, t_in_degree[n], t_out_degree[n]]
+		else:
+			res.loc[len(res)] = [n, 0, 0, 0, t_in_degree[n], t_out_degree[n]]
 res.to_csv("spammer_side.csv", index=False)
